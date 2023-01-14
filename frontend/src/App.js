f1517755906c42a7dab5b09e16b39054f6b1ba30
VaibhavR02 <vaibhavrandale800@gmail.com>
2023-01-14 16:34:53 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <a href="/">amazon</a>
+        <main>List Products</main>
+      </header>
+    </div>
+  );
+}
+
+export default App;
